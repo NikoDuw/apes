5c1d767631198e26e5606feeeacd6a638f4bcb7a
NikoDuw <bndm1410@gmail.com>
2022-06-28 00:40:39 -0500

    new components added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
+import React, { useEffect, useState } from "react";
+
 import "./App.css";
+import Header from "./components/Header";
+import CharacterTable from "./components/CharacterTable";
+import Search from "./components/Search";
+
+import axios from "axios";
+
+const hash = "d30e6c7f6fdd550f6371a2cf63340545";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const fetch = async () => {
+      if (query === "") {
+        const result = await axios(
+          `http://gateway.marvel.com/v1/public/characters?ts=1&apikey=adf42d9f9722d0473bbfc033269360e0&hash=${hash}`
+        );
+        console.log(result.data.data.results);
+        setItems(result.data.data.results);
+        setLoading(false);
+      } else {
+        const result = await axios(
+          `http://gateway.marvel.com/v1/public/characters?nameStartsWith=${query}&ts=1&apikey=adf42d9f9722d0473bbfc033269360e0&hash=${hash}`
+        );
+        console.log(result.data.data.results);
+        setItems(result.data.data.results);
+        setLoading(false);
+      }
+    };
+    fetch();
+  }, [query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>APES</h1>
-      </header>
+    <div className="container">
+      <Header />
+      <Search search={(q) => setQuery(q)} />
+      <CharacterTable items={items} isLoading={isLoading} />
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+const Search = (search) => {
+  const [text, setText] = useState();
+  const onSearch = (q) => {
+    setText(q);
+    search(q);
+  };
+  return (
+    <>
+      <section className="search">
+        <form>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Busca tu comic"
+            autoFocus
+            onChange={(e) => onSearch(e.target.value)}
+            value={text}
+          />
+        </form>
+      </section>
+      ;
+    </>
+  );
+};
+
+export default Search;
